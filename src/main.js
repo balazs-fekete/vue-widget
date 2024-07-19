97a3b7304221c4c02779686e5f5d668afb66c55a
Balazs Fekete <balazsf@growmail.com>
2024-07-19 16:18:37 +0300

    Fixed deploy issues #2

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-import { createApp, defineCustomElement } from 'vue'
+import { defineCustomElement } from 'vue'
 import './style.css'
 import App from './App.vue'
 
 const VueWidget = defineCustomElement(App)
 
 customElements.define('vue-widget', VueWidget)
-
-const app = createApp(App)
-app.mount('#app')
