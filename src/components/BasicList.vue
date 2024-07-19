8b95296ba0f3fa53a2656fdd3dde9c655f3e1d90
Bibo <balazsb@growmail.com>
2024-07-19 15:21:28 +0300

    #2 commit

---
+++ src/components/BasicList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="fetchPokemons">Refresh</button>
+    <ul>
+      <li v-for="pokemon in pokemons" :key="pokemon.name">
+        {{ pokemon.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const pokemons = ref([]);
+
+async function fetchPokemons() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+  const data = await response.json();
+  pokemons.value = data.results;
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import vueCustomElement from "vue-custom-element";
 
-createApp(App).mount('#app')
+Vue.use(vueCustomElement);
+Vue.customElement("vue-widget", App);
