293b57abb339701883744565bed5a33a4727a5ad
Balazs Fekete <balazsf@growmail.com>
2024-07-25 11:57:36 +0300

    AMP-6883 Added Loader component

--- src/components/ui/Summary.vue
+++ src/components/ui/Summary.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-4 flex justify-between items-center border-t mt-4">
+  <div class="py-6 flex justify-between items-center border-t mt-4">
     <div class="leading-8">
       <h3 class="font-medium">Full Service Price per piece:</h3>
       <p class="font-light text-sm">includes everything, even postage!</p>
